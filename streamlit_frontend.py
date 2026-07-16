edb69859d3e646ab4a61b8deac0c64913b21045d
Your Name <your-email@example.com>
2026-07-16 21:08:41 +0530

    Initial commit: LangGraph chatbot project

---
+++ streamlit_frontend.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+user_input = st.chat_input("Type your message here...")
+CONFIG = {'configurable': {'thread_id': 'thread_1'}}
+
+if 'messages_history' not in st.session_state:
+    st.session_state['messages_history'] = []
+
+for message in st.session_state['messages_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+if user_input:
+    with st.chat_message("user"):
+        st.session_state['messages_history'].append({'role': 'user', 'content': user_input})
+        st.text(user_input)
+    
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+
+    ai_message = response['messages'][-1].content
+    with st.chat_message("assistant"):
+        st.session_state['messages_history'].append({'role': 'assistant', 'content': ai_message})
+        st.text(ai_message)
+    
+    # with st.chat_message("assistant"):
+    #     st.session_state['messages_history'].append({'role': 'assistant', 'content': user_input})
+    #     st.text(user_input)
+
+
+
+
+# with st.chat_message("user"):
+#     st.text("Hi")
+
+# with st.chat_message("assistant"):
+#     st.text("Hello! How can I assist you today?")
+
+# with st.chat_message("user"):
+#     st.text("My Name is Trupti..")
+
+# user_input = st.chat_input("Type your message here...")
+
+# if user_input:
+#     with st.chat_message("user"):   
+#         st.text(user_input)
